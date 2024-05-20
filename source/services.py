0e1c76149ccf4fcac5333ab2067481d1170e868f
khloud228 <khloud228@yandex.ru>
2024-05-20 14:34:50 +0300

    save method

--- source/services.py
+++ source/services.py
@@ -25,11 +25,11 @@ def prepoplated_slug(sender, instance, *args, **kwargs):
 
 class Cart:
     def __init__(self, request):
-        """
-        
-        """
         self.session = request.session
         cart = self.session.get('cart')
         if not cart:
             cart = self.session['cart'] = {}
         self.cart = cart
+    
+    def save(self):
+        self.session.modified = True
